a2329f985f6bc8ff2005643870808267354d147f
Christoffer Sundbom <christoffer_karlsson@live.se>
2026-05-07 10:34:33 +0200

    asd

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,23 +1,9 @@
 const express = require('express');
 const path = require('path');
 const router = express.Router();
-const dashboardModel = require('../models/dashboard');
-const { getSession } = require('../services/auth');
 
-router.get('/', async (req, res) => {
-  try {
-    const token = req.cookies && req.cookies.session_token;
-    const session = token ? getSession(token) : null;
-    if (!session) {
-      const defaultDb = await dashboardModel.getDefault();
-      if (defaultDb) {
-        return res.sendFile(path.join(__dirname, '..', 'views', 'public-dashboard.html'));
-      }
-    }
-    res.sendFile(path.join(__dirname, '..', 'views', 'dashboard.html'));
-  } catch {
-    res.sendFile(path.join(__dirname, '..', 'views', 'dashboard.html'));
-  }
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'views', 'dashboard.html'));
 });
 
 router.get('/d/:slug', (req, res) => {
